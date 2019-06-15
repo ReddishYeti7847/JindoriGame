c13eb514260d9a137802b54d6c2b00f991e4d288
ReddishYeti7847 <noriyasu200081@gmail.com>
2019-06-15 16:11:56 +0900

    都道府県のリストを作成した。都道府県の番号を取得できるようにした。

--- app/src/main/java/com/example/jindorigame/MapsActivity.java
+++ app/src/main/java/com/example/jindorigame/MapsActivity.java
@@ -16,12 +16,16 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+
+    List<String> area = new ArrayList<String>();
+    int aNumber;    //取得した都道府県の番号
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +35,54 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        area.add("北海道");
+        area.add("青森県");
+        area.add("岩手県");
+        area.add("宮城県");
+        area.add("秋田県");
+        area.add("山形県");
+        area.add("福島県");
+        area.add("茨城県");
+        area.add("栃木県");
+        area.add("群馬県");
+        area.add("埼玉県");
+        area.add("千葉県");
+        area.add("東京都");
+        area.add("神奈川県");
+        area.add("新潟県");
+        area.add("富山県");
+        area.add("石川県");
+        area.add("福井県");
+        area.add("山梨県");
+        area.add("長野県");
+        area.add("岐阜県");
+        area.add("静岡県");
+        area.add("愛知県");
+        area.add("三重県");
+        area.add("滋賀県");
+        area.add("京都府");
+        area.add("大阪府");
+        area.add("兵庫県");
+        area.add("奈良県");
+        area.add("和歌山県");
+        area.add("鳥取県");
+        area.add("島根県");
+        area.add("岡山県");
+        area.add("広島県");
+        area.add("山口県");
+        area.add("徳島県");
+        area.add("香川県");
+        area.add("愛媛県");
+        area.add("高知県");
+        area.add("福岡県");
+        area.add("佐賀県");
+        area.add("長崎県");
+        area.add("熊本県");
+        area.add("大分県");
+        area.add("宮崎県");
+        area.add("鹿児島県");
+        area.add("沖縄県");
     }
 
 
@@ -52,10 +104,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
 
+//        Toast.makeText(MapsActivity.this, );
+
         mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
             @Override
             public void onMapLongClick(LatLng latLng) {
-                Toast.makeText(MapsActivity.this, getAddress(MapsActivity.this, latLng.latitude, latLng.longitude), Toast.LENGTH_SHORT).show();
+//                Toast.makeText(MapsActivity.this, getAddress(MapsActivity.this, latLng.latitude, latLng.longitude), Toast.LENGTH_SHORT).show();
+                aNumber = getAreaNumber(getAddress(MapsActivity.this, latLng.latitude, latLng.longitude));
+                Toast.makeText(MapsActivity.this, String.valueOf(aNumber), Toast.LENGTH_SHORT).show();
             }
         });
     }
@@ -80,5 +136,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         catch (Exception e){
             return "表示できません";
         }
+    }
+
+    public int getAreaNumber(String str){
+        if (area.contains(str) == false){
+            return -1;
+        }
+
+        return area.indexOf(str) + 1;
     }
 }
